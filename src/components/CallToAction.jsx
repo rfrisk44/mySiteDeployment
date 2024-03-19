283d84cfd39209e072bad6bcbf1512dd5074c021
rfrisk44 <Thefrisk4498@gmail.com>
2024-03-19 17:11:55 -0600

    Mobile menu fix

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -12,7 +12,7 @@ const CallToAction = () => {
 
   return (
     <Container className="p-3 text-center" id="landing" fetchpriority="high" decoding="async">
-      <Container className="successText">
+      <Container>
         {showModal ? (
           <div>
             <h1 style={{ fontSize: '400%' }}>Start Today</h1>
